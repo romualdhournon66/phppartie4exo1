37caa1f47100bfa37e301875e2e094aff957b3fe
romualdhournon66 <k2001@cegetel.net>
2018-11-19 13:36:30 +0100

    mon index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="FR">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <title>PHP Partie 4 exercice 1</title>
+    </head>
+    <body>
+        <div align="center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <h1 align="center">PHP Partie 4 exercice 1</h1>
+                    </div>
+                </div>
+                <div align="center">
+                    <p class="text">Faire une fonction qui retourne true.</p>
+                </div>
+                <?php
+                function returnTrue(){
+                    return true;
+                }
+                echo returnTrue();
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
